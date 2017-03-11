523857c2cb111347f66c259e933729a9c682d132
jjelfs <jjelfs@travelrepublic.co.uk>
2017-03-11 12:49:24 +0000

    chpt 4 first exercises

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -3,7 +3,9 @@ module FileOperations where
 import Prelude
 
 import Data.Path (Path, ls)
-import Data.Array (concatMap, (:))
+import Data.Array (concatMap, (:), null)
+import Data.Array.Partial (tail, head)
+import Partial.Unsafe (unsafePartial)
 
 allFiles :: Path -> Array Path
 allFiles root = root : concatMap allFiles (ls root)
@@ -12,3 +14,15 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+isEven 0 = true
+isEven n = if n < 0 then false else isEven (n-2)
+
+numEven :: Array Int -> Int
+numEven arr = 
+  if null arr then
+    0
+  else
+    if unsafePartial head arr # isEven
+      then 1 + (numEven (unsafePartial tail arr))
+      else numEven (unsafePartial tail arr)
